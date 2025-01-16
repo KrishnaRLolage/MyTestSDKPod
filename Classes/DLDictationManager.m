a6fdd1d8a38d12c4648c913fffc317cd5b010455
Krishna <krishna.lolage@microsoft.com>
2025-01-16 17:49:51 +0530

    commented SDK code

--- Classes/DLDictationManager.m
+++ Classes/DLDictationManager.m
@@ -6,10 +6,10 @@
 //
 
 #import "DLDictationManager.h"
-#import <DragonMedicalSpeechKit/DragonMedicalSpeechKit.h>
+//#import <DragonMedicalSpeechKit/DragonMedicalSpeechKit.h>
 
 @interface DLNuanceDictationManager ()
-@property (nonatomic, strong)NUSAVuiController *vuiController;
+//@property (nonatomic, strong)NUSAVuiController *vuiController;
 @end
 
 @implementation DLNuanceDictationManager
@@ -23,20 +23,20 @@
     return self;
 }
 
--(void)openSession {
-    NSString* partnerGuid = @"30462619-908d-4685-bd64-6389b4d6b368";
-    NSString* licenseGuid = @"D2321493-FC8B-4D2B-89F8-E4A7582EF687";
-    
-    [[NUSASession sharedSession] setServerURL: @"https://sas-qaq1.nuancehce.com"];
-    
-    [[NUSASession sharedSession] openForApplication:@"testcocoapodapp" partnerGuid:partnerGuid licenseGuid:licenseGuid userId:@"testuser"];
-    NSLog(@"Open Session");
-}
-
--(void)initVUI:(UIView *)vw {
-    self.vuiController = [[NUSAVuiController alloc] initWithView:vw];
-    [_vuiController open];
-    NSLog(@"Init VUI");
-}
+//-(void)openSession {
+//    NSString* partnerGuid = @"30462619-908d-4685-bd64-6389b4d6b368";
+//    NSString* licenseGuid = @"D2321493-FC8B-4D2B-89F8-E4A7582EF687";
+//    
+//    [[NUSASession sharedSession] setServerURL: @"https://sas-qaq1.nuancehce.com"];
+//    
+//    [[NUSASession sharedSession] openForApplication:@"testcocoapodapp" partnerGuid:partnerGuid licenseGuid:licenseGuid userId:@"testuser"];
+//    NSLog(@"Open Session");
+//}
+//
+//-(void)initVUI:(UIView *)vw {
+//    self.vuiController = [[NUSAVuiController alloc] initWithView:vw];
+//    [_vuiController open];
+//    NSLog(@"Init VUI");
+//}
 
 @end
